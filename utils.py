0dc2d25983642e90a64ab2ac415a471196b5819a
Bharath Ghanta <mail@bharath-g.in>
2025-04-08 16:09:08 +0530

    Added Working copy of cnn cancer detection assignment jupyter notebook

--- utils.py
+++ utils.py
@@ -4,10 +4,11 @@ import numpy as np
 import os
 import pandas as pd
 from pathlib import Path
+import tensorflow as tf
 
 
-def load_training_labels(root_dir='./', num_samples=None, balanced=False):
-    training_labels = pd.read_csv(os.path.join(root_dir, 'train_labels.csv'))
+def load_training_labels(data_dir='./', num_samples=None, balanced=False):
+    training_labels = pd.read_csv(os.path.join(data_dir, 'train_labels.csv'))
     training_labels['label'] = training_labels['label'].astype('bool')
     if num_samples is None:
         return training_labels.sample(frac=1).reset_index(drop=True)
@@ -22,21 +23,13 @@ def load_training_labels(root_dir='./', num_samples=None, balanced=False):
     return training_labels
 
 
-def get_all_training_images(training_labels, root_dir='./'):
-    return np.array([load_training_image(id, root_dir) for id in training_labels['id']])
-
-
-def load_image_file(filename):
-    img = keras.utils.load_img(filename, target_size=(96, 96, 3))
-    return keras.utils.img_to_array(img) 
-
-
-def load_training_image(id, root_dir='./'):
-    return load_image_file(os.path.join(root_dir, 'train', f'{id}.tif'))
-
-
-def load_test_image(id, root_dir='./'):
-    return load_image_file(os.path.join(root_dir, 'test', f'{id}.tif'))
+def get_training_images(training_labels, data_dir='./', color_mode='rgb', crop=False):
+    images = np.array(
+        [tf.image.central_crop(
+            keras.utils.img_to_array(keras.utils.load_img(os.path.join(data_dir, 'train', f'{id}.tif'), color_mode=color_mode)),
+            1/3 if crop else 1).numpy()
+         for id in training_labels['id']])
+    return images
 
 
 def batch_image_generator(training_labels, batch_size):
@@ -45,26 +38,34 @@ def batch_image_generator(training_labels, batch_size):
         random.shuffle(inp)
         for i in range(0, len(inp), batch_size):
             inds = inp[i:i+batch_size]
-            ids = training_labels.loc[inds]['id']
-            batch_labels = training_labels.loc[inds]['label']
-            batch_images = np.array([load_training_image(id) for id in ids])
-            yield batch_images, batch_labels
+            batch_labels = training_labels.loc[inds].reset_index(drop=True)
+            batch_images = get_training_images(batch_labels)
+            yield batch_images, batch_labels['label']
 
 
 def stochastic_batch_image_generator(training_labels, batch_size):
     while True:
-        batch_labels = training_labels.sample(batch_size)
+        batch_labels = training_labels.sample(batch_size).reset_index(drop=True)
         ids = batch_labels['id']
-        batch_images = np.array([load_training_image(id) for id in ids])
+        batch_images = get_training_images(batch_labels)
         yield batch_images, batch_labels['label']
 
 
-def generate_submission(model, root_dir, output_file='submission.csv'):
-    test_image_files = [f for f in os.listdir(os.path.join(root_dir, "test")) if f.endswith(".tif")]
-    test_images = np.array([load_test_image(Path(f).stem, root_dir) for f in test_image_files])
+def get_test_images(data_dir='./', color_mode='rgb', crop=False):
+    test_image_files = [f for f in os.listdir(os.path.join(data_dir, "test")) if f.endswith(".tif")]
+    test_ids = [Path(f).stem for f in test_image_files]
+    test_images = np.array(
+        [tf.image.central_crop(
+            keras.utils.img_to_array(keras.utils.load_img(os.path.join(data_dir, 'test', f), color_mode=color_mode)),
+            1/3 if crop else 1).numpy()
+         for f in test_image_files])
+    return test_images, test_ids
+
+
+def generate_submission(model, test_images, test_ids, color_mode='rgb', output_file='submission.csv'):
     test_predictions = model.predict(test_images)
-    test_predictions = (test_predictions > 0.5).astype("int32")
-    # Create a submission DataFrame
-    submission = pd.DataFrame({"id": [Path(f).stem for f in test_image_files], "label": test_predictions.flatten()})
-    # Save the submission DataFrame to a CSV file
-    submission.to_csv(output_file, index=False)
+    submission_probs = pd.DataFrame({"id": test_ids, "label": test_predictions.flatten()})
+    binary_predictions = (test_predictions > 0.5).astype("int32")
+    submission_binary = pd.DataFrame({"id": test_ids, "label": binary_predictions.flatten()})
+    submission_probs.to_csv(output_file, index=False)
+    submission_binary.to_csv(output_file.replace('.csv', '_binary.csv'), index=False)
