74f865ee6a76dcbc6394172928e78ae5026bf604
Bharath Ghanta <mail@bharath-g.in>
2025-04-12 20:49:51 +0530

    Added the final jupyter notebook

--- download_data.sh
+++ download_data.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# TODO Provide alternate path for downloading data
-#      Hide the kaggle credentials in a config file (with dummy sample)
-mkdir -p ~/.kaggle
-echo '{"username":"tvuser","key":"d53f35c01dcc2be525eba8c0c36b9378"}' > ~/.kaggle/kaggle.json
-chmod 600 ~/.kaggle/kaggle.json
 kaggle competitions download -c histopathologic-cancer-detection
 unzip histopathologic-cancer-detection.zip
 rm histopathologic-cancer-detection.zip
